795986080c238a94880ff37c4030fe6eedfb734b
ESIEAjochaud <31886863+ESIEAjochaud@users.noreply.github.com>
2018-01-24 13:21:30 +0100

    debug

--- CPlant.cpp
+++ CPlant.cpp
@@ -7,7 +7,7 @@ CPlant::CPlant()
 
 CPlant::CPlant(int id, short type, std::string nom, short HP, short vit, short att, short def)
 {
-	CMonster(int id, short type, std::string nom, short HP, short vit, short att, short def);
+	CMonster(id, type, nom, HP, vit, att, def);
 }
 
 CPlant::~CPlant()
