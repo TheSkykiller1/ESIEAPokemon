1eb3edde1f5a55ad44b3a15200cf0f6adb330b0c
ESIEAjochaud <31886863+ESIEAjochaud@users.noreply.github.com>
2018-01-24 22:13:02 +0100

    prise en charge de la paralysie et des attaques des pokemons foudre

--- CElectric.cpp
+++ CElectric.cpp
@@ -21,11 +21,26 @@ CElectric::~CElectric()
 
 bool CElectric::paralyze()
 {
-	return false; //todo paralyser
+	short val = m_paralysis + rand() / RAND_MAX;
+	if (val)
+	{
+		return 0;
+	}
+	else
+	{
+		return 1;
+	}
 }
 
 bool CElectric::attaquer(short num_att, CMonster &cible)
 {
+	if (m_etat == 3) //paralysie
+	{
+		if (rand() % 4 == 0) //proba de 25% de ne pas pouvoir attaquer
+		{
+			return 1;
+		}
+	}
 	if (m_nu[num_att] > 0)
 	{
 		short val_degat = degat(num_att, cible);
@@ -33,6 +48,13 @@ bool CElectric::attaquer(short num_att, CMonster &cible)
 		short HP2 = HP1 - val_degat;
 		cible.setHP(HP2);
 		m_nu[num_att]--;
+		if (paralyze()) //si l'ennemi doit être paralysé
+		{
+			if (cible.getEtat() == 0) //si l'ennemi est dans un etat normal
+			{
+				cible.setEtat(3); //paralysie
+			}
+		}
 		return 0;
 	}
 	else
