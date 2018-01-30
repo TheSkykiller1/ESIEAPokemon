867732120b1dc779623651169f3e679e7fd4736a
ESIEAjochaud <31886863+ESIEAjochaud@users.noreply.github.com>
2018-01-30 22:29:38 +0100

    modification main

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main()
 {
 	int id = 0;
 	CWorld Monde;
-	std::cout << "\t\t\t Bienvenue dans PokeLeague \n\
+	std::cout << "\n\n\t\t\t Bienvenue dans PokeLeague \n\
 Que voulez-vous faire : \n\
 \t 1) Lancer une partie \n\
 \t 2) Quitter\n";
@@ -32,9 +32,11 @@ Que voulez-vous faire : \n\
 		Monde.set_dimension(tailleX, tailleY);
 		Monde.generate_terrain();
 		Monde.map_view_terrain();
+		system("pause");
+		system("cls");
 
 		std::cout << "\t\t\t Creation des personnages \n\
-\t Choisir le nombre de personnages:";
+\t Choisir le nombre de personnages: ";
 		int nb;
 		std::cin >> nb;
 		std::cout << "\t Veuillez renseigner les différents champs: \n";
@@ -43,7 +45,7 @@ Que voulez-vous faire : \n\
 			std::string name; int posX, posY;
 			std::cout << "Joueur " << i+1 << " Nom: ";
 			std::cin >> name;
-			std::cout << "Joueur " << i+1 << " " << name << " X: ";
+			std::cout << "\nJoueur " << i+1 << " " << name << " X: ";
 			std::cin >> posX;
 			std::cout << "Joueur " << i+1 << " " << name << " Y: ";
 			std::cin >> posY;
@@ -88,10 +90,12 @@ Que voulez-vous faire : \n\
 		for (int i = 0; i < nb; i++)
 		{
 			system("cls");
-			std::cout << "Choix des objets :\n";
-			for (int i2 = 0; i2 < Monde.ListeObject.size(); i2++)
+			std::cout << "\t\tChoix des objets :\n";
+			int i1 = 0;
+			while (i1< Monde.ListeObject.size())
 			{
-				std::cout << "Nom : " << Monde.ListeObject[i2].getNom() << " Genre :" << Monde.ListeObject[i2].getGenre() << " Att : " << Monde.ListeObject[i2].getAtt() << " Def : " << Monde.ListeObject[i2].getDef() << " Vit : " << Monde.ListeObject[i2].getVit() << " HP : " << Monde.ListeObject[i2].getHP() << "\n";
+				std::cout << "Nom : " << Monde.ListeObject[i1].getNom() << " Genre :" << Monde.ListeObject[i1].getGenre() << " Att : " << Monde.ListeObject[i1].getAtt() << " Def : " << Monde.ListeObject[i1].getDef() << " Vit : " << Monde.ListeObject[i1].getVit() << " HP : " << Monde.ListeObject[i1].getHP() << "\n";
+				i1++;
 			}
 			system("pause");
 		}
