8d75084ee3165909640350b9e2e6c8df4b5890c5
Arnaud BORIO <acqua.rium@hotmail.fr>
2018-01-30 23:22:23 +0100

    Test

--- main.cpp
+++ main.cpp
@@ -177,7 +177,7 @@ int main()
 	/*CPlayer Sacha("Sacha", true, 0, 0);
 	Sacha.add_pokemon(FEU, "Salameche", 10, 50, 254, 5);
 	Sacha.add_pokemon(FEU, "salo", 10, 50, 254, 5);
-	Sacha.add_pokemon(FEU, "sali", 11, 51, 25, 5);
+	Sacha.add_pokemon(FEU, "sali", 11, 51, 25, 5);g
 	Sacha.add_pokemon(FEU, "salut", 12, 52, 15, 5);
 	Sacha.add_pokemon(FEU, "salto", 13, 53, 45, 5);
 	Sacha.add_pokemon(FEU, "saloute", 14, 54, 65, 5);
