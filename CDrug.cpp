b559620b26c1a9613e45bb51b6deac5fef492f43
Arnaud BORIO <acqua.rium@hotmail.fr>
2018-01-30 18:45:11 +0100

    Clean code

--- CDrug.cpp
+++ CDrug.cpp
@@ -1,27 +1,13 @@
 #include "CDrug.h"
 
-CDrug::CDrug()
-{
-	CObject();
-}
-
+CDrug::CDrug() { CObject(); }
 CDrug::CDrug(int id,std::string genre, std::string nom, std::vector<std::string> type, std::string etat)
 {
 	CObject(id, genre, nom, type);
 	m_etat = etat;
 }
+CDrug::~CDrug(){}
 
-CDrug::~CDrug()
-{
-
-}
-
-std::string CDrug::getEtat()
-{
-	return m_etat;
-}
-
-void CDrug::setEtat(std::string etat)
-{
-	m_etat = etat;
-}
+//Methodes
+std::string CDrug::getEtat() { return m_etat; }
+void CDrug::setEtat(std::string etat) { m_etat = etat; }
--- CPotion.cpp
+++ CPotion.cpp
@@ -1,10 +1,6 @@
 #include "CPotion.h"
 
-CPotion::CPotion()
-{
-	CObject();
-}
-
+CPotion::CPotion(){CObject();}
 CPotion::CPotion(int id, std::string genre, std::string nom, std::vector<std::string> type, int vit, int att, int def, int HP)
 {
 	CObject(id,genre, nom, type);
@@ -13,52 +9,16 @@ CPotion::CPotion(int id, std::string genre, std::string nom, std::vector<std::st
 	m_def = def;
 	m_HP = HP;
 }
-
-CPotion::~CPotion()
-{
-
-}
+CPotion::~CPotion(){}
 
 // GETTERS
-
-int CPotion::getVit()
-{
-	return m_vit;
-}
-
-int CPotion::getAtt()
-{
-	return m_att;
-}
-
-int CPotion::getDef()
-{
-	return m_def;
-}
-
-int CPotion::getHP()
-{
-	return m_HP;
-}
+int CPotion::getVit(){return m_vit;}
+int CPotion::getAtt(){return m_att;}
+int CPotion::getDef(){return m_def;}
+int CPotion::getHP(){return m_HP;}
 
 // SETTERS
-
-void CPotion::setVit(int vit)
-{
-	m_vit = vit;
-}
-
-void CPotion::setAtt(int att)
-{
-	m_att = att;
-}
-
-void CPotion::setDef(int def)
-{
-	m_def = def;
-}
-
-void CPotion::setHP(int HP)
-{
-	m_HP = HP;
-}
+void CPotion::setVit(int vit){m_vit = vit;}
+void CPotion::setAtt(int att){m_att = att;}
+void CPotion::setDef(int def){m_def = def;}
+void CPotion::setHP(int HP){m_HP = HP;}
